3c4e38cdbfdead980b17e3e8da33ae16b114a8f5
manishsharma1997 <ms097ms@gmail.com>
2022-04-22 15:38:28 +0530

    first

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 var connect=()=>{
-    const url = "mongodb://127.0.0.1:27017/clients"
+    const url = "mongodb+srv://root:Msharma27@@cluster0.oyccw.mongodb.net/clients?retryWrites=true&w=majority"
     return mongoose.connect(url,{useNewUrlParser:true}, (err,data)=>{
         if(err){throw err}
         else{return console.log('Database Connected Successfully')}
